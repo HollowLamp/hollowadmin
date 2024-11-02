7ee97d9dd08c02730926c37fc594f76afba3b37b
hollowlamp <2504384479@qq.com>
2024-11-02 13:26:56 +0800

    feat: 配置端口

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    server: {
+      port: parseInt(env.VITE_PORT || "3000"),
     },
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
